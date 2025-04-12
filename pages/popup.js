70e89025242ca2fa0d88391cb5e2ea6859c0e476
basyura <basyura@gmail.com>
2025-04-12 11:17:08 +0900

    format

--- pages/popup.js
+++ pages/popup.js
@@ -1,59 +1,71 @@
-var pause = document.getElementById('pause'),
-    blacklist = document.getElementById('blacklist'),
-    settings = document.getElementById('settings'),
-    isEnabled = true,
-    isBlacklisted = false;
+var pause = document.getElementById("pause"),
+  blacklist = document.getElementById("blacklist"),
+  settings = document.getElementById("settings"),
+  isEnabled = true,
+  isBlacklisted = false;
 
-var port = chrome.runtime.connect({name: 'popup'});
-port.onMessage.addListener(function(data) {
+var port = chrome.runtime.connect({ name: "popup" });
+port.onMessage.addListener(function (data) {
   if (data === true) {
-    blacklist.textContent = 'Enable cVim on this domain';
+    blacklist.textContent = "Enable cVim on this domain";
     isBlacklisted = true;
   }
 });
-port.postMessage({action: 'getBlacklisted'});
+port.postMessage({ action: "getBlacklisted" });
 
-chrome.runtime.sendMessage({action: 'getActiveState'}, function(response) {
+chrome.runtime.sendMessage({ action: "getActiveState" }, function (response) {
   isEnabled = response;
   if (isEnabled) {
-    pause.textContent = 'Disable cVim';
+    pause.textContent = "Disable cVim";
   } else {
-    pause.textContent = 'Enable cVim';
+    pause.textContent = "Enable cVim";
   }
   return true;
 });
 
-settings.addEventListener('click', function() {
-  chrome.runtime.sendMessage({
-    action: 'openLinkTab',
-    active: true,
-    url: chrome.runtime.getURL('/pages/options.html')
-  });
-}, false);
-
-pause.addEventListener('click', function() {
-  isEnabled = !isEnabled;
-  if (isEnabled) {
-    pause.textContent = 'Disable cVim';
-  } else {
-    pause.textContent = 'Enable cVim';
-  }
-  port.postMessage({action: 'toggleEnabled', blacklisted: isBlacklisted});
-}, false);
-
-blacklist.addEventListener('click', function() {
-  isBlacklisted = !isBlacklisted;
-  if (blacklist.textContent === 'Disable cVim on this domain') {
-    blacklist.textContent = 'Enable cVim on this domain';
-  } else {
-    blacklist.textContent = 'Disable cVim on this domain';
-  }
-  port.postMessage({action: 'toggleBlacklisted'});
-  if (isEnabled) {
-    port.postMessage({
-      action: 'toggleEnabled',
-      singleTab: true,
-      blacklisted: isBlacklisted
+settings.addEventListener(
+  "click",
+  function () {
+    chrome.runtime.sendMessage({
+      action: "openLinkTab",
+      active: true,
+      url: chrome.runtime.getURL("/pages/options.html"),
     });
-  }
-}, false);
+  },
+  false
+);
+
+pause.addEventListener(
+  "click",
+  function () {
+    isEnabled = !isEnabled;
+    if (isEnabled) {
+      pause.textContent = "Disable cVim";
+    } else {
+      pause.textContent = "Enable cVim";
+    }
+    port.postMessage({ action: "toggleEnabled", blacklisted: isBlacklisted });
+  },
+  false
+);
+
+blacklist.addEventListener(
+  "click",
+  function () {
+    isBlacklisted = !isBlacklisted;
+    if (blacklist.textContent === "Disable cVim on this domain") {
+      blacklist.textContent = "Enable cVim on this domain";
+    } else {
+      blacklist.textContent = "Disable cVim on this domain";
+    }
+    port.postMessage({ action: "toggleBlacklisted" });
+    if (isEnabled) {
+      port.postMessage({
+        action: "toggleEnabled",
+        singleTab: true,
+        blacklisted: isBlacklisted,
+      });
+    }
+  },
+  false
+);
